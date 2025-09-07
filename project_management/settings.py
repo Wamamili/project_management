31665d7af10bea46cc09d31abf4e055e525302e9
Clinton Wamamili <43131448+Wamamili@users.noreply.github.com>
2025-09-07 23:40:40 +0300

    settings.py

--- project_management/settings.py
+++ project_management/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'insecure-secret-for-dev-only')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://project-management-svj3.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
